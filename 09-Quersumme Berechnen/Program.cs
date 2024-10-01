4cf297e62eca6c7180b8e2dc986647d581170354
carnevio <nevio.carcangiu@icloud.com>
2024-10-01 16:07:43 +0200

    Aufgabe 9 fertig

---
+++ 09-Quersumme Berechnen/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_Quersumme_Berechnen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Bitte eine Zahl eingeben:");
+            int zahl = int.Parse(Console.ReadLine());
+
+            int quersumme = BerechneQuersumme(zahl);
+
+            Console.WriteLine("Die Quersumme von " + zahl + " ist: " + quersumme);
+        }
+        static int BerechneQuersumme(int zahl)
+        {
+            int sum = 0;
+
+            while (true)
+            {
+
+
+                if (zahl != 0)
+                {
+                    sum = sum + (zahl % 10);
+                    zahl = zahl / 10;
+                }
+
+                else
+                {
+                    return sum;
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ 09-Quersumme Berechnen/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("09-Quersumme Berechnen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("09-Quersumme Berechnen")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("abc6be7b-03f0-4122-b8de-72c4807f3ebe")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
