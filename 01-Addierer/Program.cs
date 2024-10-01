8f9d5a75033e24edfb589f493b3ce87549a10c56
carnevio <nevio.carcangiu@icloud.com>
2024-10-01 09:15:06 +0200

    Aufgabe 1 fertig

--- 01-Addierer/Program.cs
+++ 01-Addierer/Program.cs
@@ -10,7 +10,18 @@ namespace _01_Addierer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("01");
+            Console.WriteLine("Dieses Programm berechnet die Summe von zwei Zahlen.");
+
+            Console.Write("Zahl 1:");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Zahl 2:");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            int summe = zahl1 + zahl2; 
+
+            Console.Write("Summe: " + summe);
+
             Console.ReadKey();
         }
     }
