b6fd57486402af12658b97c1de335c75e7b02805
carnevio <nevio.carcangiu@icloud.com>
2024-10-01 09:54:39 +0200

    Aufgabe 2 fertig

--- 02-SekundenMonat/Program.cs
+++ 02-SekundenMonat/Program.cs
@@ -10,8 +10,37 @@ namespace _02_SekundenMonat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("02");
+            Console.WriteLine("Wie viele Tage hat der Monat, für den Sie die Sekundenzahl berechnen wollen?");
+            string input = Console.ReadLine();
+
+            int zahl = 0;
+
+            if (MyTryParse(input, out zahl) == true && zahl > 27 && zahl < 32)
+            {
+               Console.WriteLine("Der Monat hat "+  zahl * 86400 + " Sekunden."); 
+            }
+            else
+            {
+                Console.WriteLine("Eingabefehler. Bitte geben Sie einen gültigen Wert ein.");
+            }
             Console.ReadKey();
+        }
+
+
+        static bool MyTryParse(string s, out int n)
+        {
+            n = 0;
+
+            try
+            {
+                n = Convert.ToInt32(s);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+       
         }
     }
 }
