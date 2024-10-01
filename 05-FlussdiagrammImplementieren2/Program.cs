62102fe86009404844af47c89331f2e64df77d8b
carnevio <nevio.carcangiu@icloud.com>
2024-10-01 11:41:11 +0200

    Aufgabe 5 fertig

---
+++ 05-FlussdiagrammImplementieren2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_FlussdiagrammImplementieren2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie viele Kilometer möchtest du rennen?");
+            int km = Convert.ToInt32(Console.ReadLine());
+
+            if (km > 42)
+            {
+                Console.WriteLine("Das schaffst du nicht!");
+            }
+            else
+            {
+                double rounds  = km / 0.4;
+                Console.WriteLine("Das sind " + rounds + " Runden. Bereit für den Lauf?");
+
+                Console.WriteLine("Wenn Ja, dann klicke die Nummer 1, sonst die nummer 2.");
+
+                int ready = Convert.ToInt32(Console.ReadLine());
+
+                if (ready == 1) 
+                {
+                    int i = 1;
+
+                    while (i <= rounds)
+                    {
+                        Console.WriteLine("Du laäufst die Runde " + i);
+                        i++;
+                    }
+                    Console.WriteLine("Du hast es geschaft");
+                }
+
+              
+            }
+        }
+    }
+}
