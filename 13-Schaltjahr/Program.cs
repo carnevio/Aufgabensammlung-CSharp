00fa1331769f69ff08a3d88b6d5c8b0d89ba129a
carnevio <nevio.carcangiu@icloud.com>
2024-10-02 11:28:13 +0200

    Aufgabe 13 fertig

---
+++ 13-Schaltjahr/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Schaltjahr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prüfen, ob es sich bei einem jahr um ein Schaltjahr handelt.");
+
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year % 4 == 0 || year % 400 == 0)
+            {
+                Console.WriteLine("Das Jahr " + year + "ist ein Schaltjahr.");
+                
+            }
+            else
+            {
+                Console.WriteLine("Das Jahr " + year + "ist KEIN Schaltjahr.");
+            }
+        }
+    }
+}
