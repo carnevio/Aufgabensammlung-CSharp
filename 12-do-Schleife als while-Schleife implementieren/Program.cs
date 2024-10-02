e2f3007ade945a9fcdba6f44c178b11a8e05af38
carnevio <nevio.carcangiu@icloud.com>
2024-10-02 11:12:49 +0200

    Aufgabe 12 fertig

---
+++ 12-do-Schleife als while-Schleife implementieren/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_do_Schleife_als_while_Schleife_implementieren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            doSomething();
+            while (i >= 0)
+            {
+                doSomething();
+            }
+        }
+    }
+}
---
+++ 12-do-Schleife als while-Schleife implementieren/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("12-do-Schleife als while-Schleife implementieren")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("12-do-Schleife als while-Schleife implementieren")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7fbe4a8c-87e1-404f-946a-879dcf324dcf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
