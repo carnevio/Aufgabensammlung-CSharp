c24d0f81f1bf908c9d3866b294959b695c6753e5
carnevio <nevio.carcangiu@icloud.com>
2024-10-02 11:04:54 +0200

    Aufgabe 11 fertig

---
+++ 11-Eine diagonale Linie zechnen/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Eine_diagonale_Linie_zechnen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wie lang solle die Linie sein?");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            int zahl = 0;
+
+
+            for (int i = 0; i < input; i++)
+            {
+
+                for (int j = 0; j < input; j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(" ");
+                        
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                    
+
+                    
+                    
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
