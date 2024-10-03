3683025364eff188234cac5941c6a4d0cb8cf29a
carnevio <nevio.carcangiu@icloud.com>
2024-10-03 09:16:49 +0200

    Aufgabe 15 fertig

--- 15-Zaheln raten/Program.cs
+++ 15-Zaheln raten/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,9 +12,35 @@ namespace _15_Zaheln_raten
         static void Main(string[] args)
         {
             Console.WriteLine("Gib eine Zahl ein, zwischen 1 und 100");
-            int zahl = Convert.ToInt32(Console.ReadLine());
+            
+            int counter = 0;
 
 
+            Random rnd = new Random();
+            int nums = rnd.Next(0, 100);
+            bool game = true;   
+            while (game == true)
+            {
+                counter++;
+                int zahl = Convert.ToInt32(Console.ReadLine());
+                if (nums < zahl)
+                {
+                    Console.WriteLine("Zu Hoch.");
+                }
+
+                else if (nums > zahl)
+                {
+                    Console.WriteLine("Zu Tief.");
+                }
+
+                else if (nums == zahl)
+                {
+                    Console.WriteLine("Die zahl Stimmt. Du hast " + counter + " Versuche gebraucht.");
+                    game = false;
+                }
+            }
         }
+
+        
     }
 }
